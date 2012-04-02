b323d0ef4f13d71a95dd5723f100d08ecc61b636
Justin Rusbatch <jrusbatch@gmail.com>
2012-04-01 21:27:27 -0400

    Rename rosie.css to compilify.css

--- Compilify/Assets/js/compilify.js
+++ Compilify/Assets/js/compilify.js
@@ -1,1 +1,11 @@
 ﻿
+(function($, CodeMirror, undefined) {
+    var root = this;
+
+    var textarea = document.getElementById('editor');
+
+    root.Editor = CodeMirror.fromTextArea(textarea, {
+        indentUnit: 4,
+        lineNumbers: true
+    });
+}).call(window, jQuery, CodeMirror);
