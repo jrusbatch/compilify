a3432ab9726b4c836802a8066508b0cb437cf31d
Justin Rusbatch <jrusbatch@gmail.com>
2012-04-05 09:33:24 -0400

    Move analytics code into the head element

--- Web/Infrastructure/Extensions/HtmlHelperExtensions.cs
+++ Web/Infrastructure/Extensions/HtmlHelperExtensions.cs
@@ -12,16 +12,16 @@ namespace Compilify.Web.Infrastructure.Extensions
             if (!string.IsNullOrWhiteSpace(accountId))
             {
                 return MvcHtmlString.Create(
-                    @"<script type=""text/javascript"">
-                      var _gaq = _gaq || [];
-                      _gaq.push(['_setAccount', '" + accountId + @"']);
-                      _gaq.push(['_trackPageview']);
+                    @"<script>
+                          var _gaq = _gaq || [];
+                          _gaq.push(['_setAccount', '" + accountId + @"']);
+                          _gaq.push(['_trackPageview']);
 
-                      (function () {
-                         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-                      })();
+                          (function () {
+                             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                          })();
                       </script>");
             }
 #endif
