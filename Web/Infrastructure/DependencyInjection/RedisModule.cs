244ab6fb0427b7c928ce2b7dab1a81f20765d63b
Justin Rusbatch <jrusbatch@gmail.com>
2012-04-06 17:29:40 -0400

    Separate the Redis URI into usable pieces

--- Web/Infrastructure/DependencyInjection/RedisModule.cs
+++ Web/Infrastructure/DependencyInjection/RedisModule.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Configuration;
+using System.Linq;
 using Autofac;
 using Autofac.Integration.Mvc;
 using BookSleeve;
@@ -17,7 +19,15 @@ namespace Compilify.Web.Infrastructure.DependencyInjection
         private static RedisConnection CreateConnection(IComponentContext context)
         {
             var connectionString = ConfigurationManager.AppSettings["REDISTOGO_URL"];
-            var connection = new RedisConnection(connectionString);
+
+            RedisConnection connection;
+#if !DEBUG
+            var uri = new Uri(connectionString);
+            var password = uri.UserInfo.Split(':').Last();
+            connection = new RedisConnection(uri.Host, uri.Port, password: password);
+#else
+            connection = new RedisConnection(connectionString);
+#endif
             connection.Wait(connection.Open());
             return connection;
         }
