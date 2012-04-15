823de4d5a8d1d9d6dedd22004874cc9148affa1a
Justin Rusbatch <jrusbatch@gmail.com>
2012-04-15 00:36:28 -0400

    fixes #27, allow saves even if content didn't change

--- Web/Assets/js/compilify.js
+++ Web/Assets/js/compilify.js
@@ -31,7 +31,7 @@ if (typeof String.prototype.trim !== 'function') {
         }
     }
     
-    function save(value) {
+    function save(code) {
         /// <summary>
         /// Save content.</summary>
         
@@ -40,15 +40,15 @@ if (typeof String.prototype.trim !== 'function') {
         return $.ajax(window.location.pathname, {
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify({ post: { Content: value } })
+            data: JSON.stringify({ post: { Content: code } })
         });
     }
-    
-    function validate(code) {
+
+    var validate = function(code) {
         /// <summary>
         /// Sends code to the server for validation and displays the resulting
         /// errors, if any.</summary>
-        
+
         trackEvent('Code', 'Validate', window.location.pathname);
 
         return $.ajax('/validate', {
@@ -63,12 +63,12 @@ if (typeof String.prototype.trim !== 'function') {
 
                     if (data.length == 0) {
                         $list.append('<li class="message success">Build ' +
-                                     'completed successfully.</li>');
+                            'completed successfully.</li>');
                     } else {
                         for (var i in msg.data) {
-                            var error = msg.data[ i ];
-                            $list.append('<li class="message error">' + 
-                                         htmlEscape(error) + '</li>');
+                            var error = msg.data[i];
+                            $list.append('<li class="message error">' +
+                                htmlEscape(error) + '</li>');
                         }
                     }
 
@@ -76,7 +76,7 @@ if (typeof String.prototype.trim !== 'function') {
                 }
             }
         });
-    }
+    };
 
     function execute(code) {
         /// <summary>
@@ -131,8 +131,10 @@ if (typeof String.prototype.trim !== 'function') {
             theme: 'neat',
             mode: 'text/x-csharp',
             onChange: _.debounce(function (sender) {
+                var code = sender.getValue().trim();
+                
                 // throttled to once every 500ms max
-                validate(sender.getValue());
+                validate(code);
             }, 500)
         });
 
@@ -140,23 +142,18 @@ if (typeof String.prototype.trim !== 'function') {
             var code = this.getValue().trim();
 
             // Only save the content if it changed since we last loaded it.
-            if (code.toUpperCase() !== original) {
-                save(code)
-                    .done(function(msg) {
-                        // Might be able to use the object stored by pushState 
-                        // hold the original value
-                        root.history.pushState({ }, '', msg.data.url);
-                        original = code.toUpperCase();
-                    });
-            }
+            save(code)
+                .done(function(msg) {
+                    // Might be able to use the object stored by pushState 
+                    // hold the original value
+                    root.history.pushState({ }, '', msg.data.url);
+                });
 
             return false;
         }, Compilify.Editor);
         
         root.CodeMirror.commands["save"] = Compilify.Editor.save;
         
-
-
         $('#define .js-save').on('click', Compilify.Editor.save);
 
         $('#define .js-execute').on('click', function() {
