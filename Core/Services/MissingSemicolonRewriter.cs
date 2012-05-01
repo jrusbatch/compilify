b09ccea5cdc956b98fd305bb0860dd7f957d8467
Justin Rusbatch <jrusbatch@gmail.com>
2012-05-01 16:39:26 -0400

    Add MissingSemicolonRewriter class

---
+++ Core/Services/MissingSemicolonRewriter.cs
@@ -0,0 +1,17 @@
+﻿using Roslyn.Compilers.CSharp;
+
+namespace Compilify.Services
+{
+    internal class MissingSemicolonRewriter : SyntaxRewriter
+    {
+        protected override SyntaxToken VisitToken(SyntaxToken token)
+        {
+            if (token.IsMissing && token.Kind == SyntaxKind.SemicolonToken)
+            {
+                return Syntax.Token(SyntaxKind.SemicolonToken);
+            }
+
+            return token;
+        }
+    }
+}
