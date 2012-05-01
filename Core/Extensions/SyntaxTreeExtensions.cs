01f9c2c2f48d784d15bac44a4873d927890f0136
Justin Rusbatch <jrusbatch@gmail.com>
2012-05-01 16:48:59 -0400

    Adding a new overload for the RewriteWith extension method

--- Core/Extensions/SyntaxTreeExtensions.cs
+++ Core/Extensions/SyntaxTreeExtensions.cs
@@ -6,7 +6,11 @@ namespace Compilify.Extensions
     {
         internal static SyntaxTree RewriteWith<TRewriter>(this SyntaxTree tree) where TRewriter : SyntaxRewriter, new()
         {
-            var rewriter = new TRewriter();
+            return tree.RewriteWith(new TRewriter());
+        }
+
+        internal static SyntaxTree RewriteWith<TRewriter>(this SyntaxTree tree, TRewriter rewriter) where TRewriter : SyntaxRewriter
+        {
             return SyntaxTree.Create(tree.FileName, (CompilationUnitSyntax)rewriter.Visit(tree.Root), tree.Options);
         }
     }
