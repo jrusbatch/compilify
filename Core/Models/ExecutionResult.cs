7b2f9846b5900c15df3b3d2f7d97d947019b4492
Justin Rusbatch <jrusbatch@gmail.com>
2012-05-05 15:42:10 -0400

    Move SandboxResult and WorkerResult into their own files

--- Core/Models/ExecutionResult.cs
+++ Core/Models/ExecutionResult.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using ProtoBuf;
 
 namespace Compilify.Models
@@ -19,48 +18,5 @@ namespace Compilify.Models
 
         [ProtoMember(4)]
         public string Result { get; set; }
-    }
-
-    [Serializable]
-    [ProtoContract]
-    public class WorkerResult
-    {
-        [ProtoMember(1)]
-        public DateTime Time { get; set; }
-
-        [ProtoMember(2)]
-        public long Duration { get; set; }
-
-        [ProtoMember(3, AsReference = true)]
-        public ExecutionResult ExecutionResult { get; set; }
-
-        public byte[] GetBytes()
-        {
-            using (var stream = new MemoryStream())
-            {
-                Serializer.Serialize(stream, this);
-                return stream.ToArray();
-            }
-        }
-
-        public static WorkerResult Deserialize(byte[] data)
-        {
-            if (data == null)
-            {
-                throw new ArgumentNullException("data");
-            }
-
-            using (var stream = new MemoryStream(data))
-            {
-                return Serializer.Deserialize<WorkerResult>(stream);
-            }
-        }
-    }
-
-    [Serializable]
-    public class SandboxResult
-    {
-        public string ConsoleOutput { get; set; }
-        public object ReturnValue { get; set; }
     }
 }
--- Core/Models/WorkerResult.cs
+++ Core/Models/WorkerResult.cs
@@ -1,8 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
+using Compilify.Extensions;
+using ProtoBuf;
 
 namespace Compilify.Models
 {
+    [Serializable]
+    [ProtoContract]
+    public class WorkerResult
+    {
+        [ProtoMember(1)]
+        public DateTime Time { get; set; }
+
+        [ProtoMember(2)]
+        public long Duration { get; set; }
+
+        [ProtoMember(3, AsReference = true)]
+        public ExecutionResult ExecutionResult { get; set; }
+
+        public string ToResultString()
+        {
+            var builder = new StringBuilder();
+
+            if (!string.IsNullOrEmpty(ExecutionResult.ConsoleOutput))
+            {
+                builder.AppendLine(ExecutionResult.ConsoleOutput);
+            }
+
+            builder.AppendLine(ExecutionResult.Result);
+
+            builder.AppendLine();
+            builder.AppendFormat("CPU Time: {0}" + Environment.NewLine, ExecutionResult.ProcessorTime);
+
+            builder.AppendFormat("Bytes Allocated: {0}" + Environment.NewLine, 
+                                 ExecutionResult.TotalMemoryAllocated.ToByteSizeString());
+
+            return builder.ToString();
+        }
+
+        public byte[] GetBytes()
+        {
+            using (var stream = new MemoryStream())
+            {
+                Serializer.Serialize(stream, this);
+                return stream.ToArray();
+            }
+        }
+
+        public static WorkerResult Deserialize(byte[] data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException("data");
+            }
+
+            using (var stream = new MemoryStream(data))
+            {
+                return Serializer.Deserialize<WorkerResult>(stream);
+            }
+        }
+    }
 }
