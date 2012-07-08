fc894cc06bf533fb38c65f1239e593ed39a68458
Justin Rusbatch <jrusbatch@gmail.com>
2012-07-08 16:14:08 -0400

    Move TaskAsyncHelper into Compilify.Utilities namespace

--- Core/Utilities/TaskAsyncHelper.cs
+++ Core/Utilities/TaskAsyncHelper.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Compilify.Web.Infrastructure
+namespace Compilify.Utilities
 {
     internal static class TaskAsyncHelper
     {
