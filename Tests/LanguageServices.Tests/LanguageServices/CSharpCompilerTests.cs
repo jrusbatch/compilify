5cdc0f6ae7cf8e5dd5829c12c6182aed78f3dfd9
Justin Rusbatch <jrusbatch@gmail.com>
2012-09-23 17:17:09 -0400

    Creating a unit test project for the LanguageServices assembly

---
+++ Tests/LanguageServices.Tests/LanguageServices/CSharpCompilerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Xunit;
+
+namespace Compilify.LanguageServices
+{
+    public class CSharpCompilerTests
+    {
+        [Fact]
+        public void CodeCanBeCompiledAndExecuted()
+        {
+            var classes = new Document("Classes", "public static void SayHello() { Console.WriteLine(\"Hello, world!\"); }");
+            var content = new Document("Content", "SayHello(); return \"Done!\";");
+
+            var command = new EvaluateCodeCommand
+                          {
+                              ClientId = Guid.NewGuid().ToString("N"),
+                              Documents = new List<Document> { content, classes },
+                              Name = "Untitled",
+                              Submitted = DateTime.UtcNow,
+                              TimeoutPeriod = TimeSpan.FromSeconds(30)
+                          };
+
+            var sandbox = new Sandbox();
+            var compiler = new CSharpCompiler();
+
+            var actual = compiler.Compile(command);
+
+            Assert.NotNull(actual);
+
+            var result = sandbox.Execute(actual, Timeout.InfiniteTimeSpan);
+
+            Assert.NotNull(result);
+            Assert.Equal("Hello, world!\r\n", result.ConsoleOutput);
+            Assert.Equal("\"Done!\"", result.Result);
+        }
+    }
+}
---
+++ Tests/LanguageServices.Tests/LanguageServices/EvaluateCodeCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ProtoBuf;
+using Xunit;
+
+namespace Compilify.LanguageServices
+{
+    public class EvaluateCodeCommandTests
+    {
+        [Fact]
+        public void CanBeSerialized()
+        {
+            var expected = new EvaluateCodeCommand
+                          {
+                              Documents = new List<Document>()
+                          };
+
+            EvaluateCodeCommand actual;
+            using (var stream = new MemoryStream())
+            {
+                Assert.DoesNotThrow(() => Serializer.Serialize(stream, expected));
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                actual = Serializer.Deserialize<EvaluateCodeCommand>(stream);
+            }
+
+            Assert.NotNull(actual);
+            Assert.Equal(0, actual.Documents.Count());
+        }
+    }
+}
