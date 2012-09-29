c3687b02052772977f16e4cd22e57f03b24623eb
Justin Rusbatch <jrusbatch@gmail.com>
2012-09-29 11:01:18 -0400

    Creating the CompilifyException class in the Core project

---
+++ Core/CompilifyException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Compilify
+{
+    [Serializable]
+    public class CompilifyException : Exception
+    {
+        public CompilifyException() { }
+
+        public CompilifyException(string message)
+            : base(message) { }
+
+        public CompilifyException(string message, Exception inner)
+            : base(message, inner) { }
+
+        protected CompilifyException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
