400e621cbc17294f367680afc6496f82e1c5aa92
Justin Rusbatch <jrusbatch@gmail.com>
2012-09-29 11:09:07 -0400

    Creating the ServiceNotFoundException clas in the Web project

---
+++ Web/Infrastructure/ServiceNotFoundException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Compilify.Web.Infrastructure
+{
+    [Serializable]
+    public class ServiceNotFoundException : CompilifyException
+    {
+        public const string DefaultMessage =
+            "A concrete implementation of the service \"{0}\" could not be found.";
+
+        private readonly Type type;
+
+        public ServiceNotFoundException(Type serviceType)
+            : this(serviceType, string.Format(DefaultMessage, serviceType.FullName)) { }
+
+        public ServiceNotFoundException(Type serviceType, string message)
+            : base(message)
+        {
+            type = serviceType;
+        }
+
+        public ServiceNotFoundException(Type serviceType, string message, Exception inner)
+            : base(message, inner)
+        {
+            type = serviceType;
+        }
+
+        protected ServiceNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+        public Type Type
+        {
+            get { return type; }
+        }
+    }
+}
